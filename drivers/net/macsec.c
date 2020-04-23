1e99e0694843cee7ea353a2d35a79b9be4856291
Dmitry Bogdanov <dbogdanov@marvell.com>
2020-04-23 13:54:20 +0300

    net:macsec: count RX bytes/packets when offloaded

--- drivers/net/macsec.c
+++ drivers/net/macsec.c
@@ -938,6 +938,7 @@ static enum rx_handler_result handle_not_macsec(struct sk_buff *skb)
 				if (!nskb)
 					break;
 
+				count_rx(ndev, nskb->len);
 				nskb->dev = ndev;
 				netif_rx(nskb);
 			} else if (ether_addr_equal_64bits(hdr->h_dest,
@@ -945,6 +946,7 @@ static enum rx_handler_result handle_not_macsec(struct sk_buff *skb)
 				/* HW offload enabled, divert skb */
 				skb->dev = ndev;
 				skb->pkt_type = PACKET_HOST;
+				count_rx(ndev, skb->len);
 				ret = RX_HANDLER_ANOTHER;
 				goto out;
 			} else if (is_multicast_ether_addr_64bits(hdr->h_dest)) {
@@ -959,6 +961,7 @@ static enum rx_handler_result handle_not_macsec(struct sk_buff *skb)
 				else
 					nskb->pkt_type = PACKET_MULTICAST;
 
+				count_rx(ndev, nskb->len);
 				netif_rx(nskb);
 			}
 			continue;
